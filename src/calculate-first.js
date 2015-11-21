876679a7a4ec3685fdcd652db7bf9a4448095860
liujing <pamelaling91@gmail.com>
2015-11-21 17:27:22 +0800

    first-test

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,3 +1,14 @@
 function single_element(arr) {
-  
+
+  var filtered=arr.filter(functon(n,i){
+    if ((i+1)%2==0)
+      return arr[i];
+  })
+for (var i = 0; i < filtered.length; i++) {
+  for (var j = i+1; i < filtered.length; j++) {
+    if (filtered[i]!=filtered[j]) {
+      return filtered[i];
+    }
+  }
+}
 }
