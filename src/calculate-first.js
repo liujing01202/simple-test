e6d67f27807b0112a907c63900fbc7312261406a
liujing <pamelaling91@gmail.com>
2015-11-27 22:21:05 +0800

    second commit

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,14 +1,9 @@
 function single_element(arr) {
-
-  var filtered=arr.filter(functon(n,i){
-    if ((i+1)%2==0)
-      return arr[i];
+  return arr.filter(function(val,key){
+    return key % 2 == 1
+  }).filter(function(val,key,newArr){
+    return newArr.indexOf(val) === newArr.lastIndexOf(val);
+    //indexOf是返回某个指定的字符串值在字符串中首次出现的位置
+    //lastIndexOf() 方法可返回一个指定的字符串值最后出现的位置
   })
-for (var i = 0; i < filtered.length; i++) {
-  for (var j = i+1; i < filtered.length; j++) {
-    if (filtered[i]!=filtered[j]) {
-      return filtered[i];
-    }
-  }
-}
 }
--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,3 +1,22 @@
+function median(arr) {
+  var newArr = arr.concat(); //复制一份，防止计算中位数时导致原数组发生改动
+  var len = newArr.length;
+  newArr.sort(function(a,b) {
+    return a - b;
+  });
+
+  if(len % 2 == 0) {
+    return (newArr[len/2-1] + newArr[len/2]) / 2;
+  } else {
+    return newArr[(len-1)/2];
+  }
+}
+
 function calculate_median(arr) {
+  var list = arr.filter(function(val, key) {
+    return key % 2 === 1;
+  })
+
+  return median(list);
 
 }
